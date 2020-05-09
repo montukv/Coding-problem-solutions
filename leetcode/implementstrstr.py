d3b87f61bd8cedf60ef8ef675ada8df5ddb90306
montukv <46618399+montukv@users.noreply.github.com>
2020-05-10 00:08:01 +0530

    Add files via upload

---
+++ leetcode/implementstrstr.py
@@ -0,0 +1,25 @@
+'''28. Implement strStr()
+Easy
+
+1434
+
+1779
+
+Add to List
+
+Share
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1'''
+haystack = input()
+needle = input()
+print(haystack.find(needle))
