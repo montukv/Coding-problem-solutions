e56c6a19d2999157c39f0c39fe283f132a413730
montukv <46618399+montukv@users.noreply.github.com>
2020-05-18 00:34:33 +0530

    Update longestCommonPrefix.py

--- leetcode/longestCommonPrefix.py
+++ leetcode/longestCommonPrefix.py
@@ -1,25 +1,14 @@
 #longest common prefix
  def longestCommonPrefix(s):
-        if s == [] :
-            return "" 
-        else :
-            try : 
-                sm = min(s, key=len)
-                n = len(sm)
-                lp = sm[0]
-                pre = ''
-                flag = True
-                for i in range(0,n):					
-                    for j in range(0,len(s)):
-                        lp = sm[i]
-                        if s[j][i] != lp :
-                            flag = False
-                    if flag == False:
-                        break ;
-                    else : 
-                        pre += lp
-                return pre
-            except Exception:
-                return ""
+   if not s:  
+            return ""
+        common = min(s,key=len) 
+        for i in s:  
+            while not i.startswith(common): 
+                common = common[:len(common)-1]
+                if not common:
+                    return ""
+        return common
+        
 s =  ["flower","flow","flight"]
 checkSmallestLength(s)
