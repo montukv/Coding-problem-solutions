3b37a0a4e8d769fe00340b7179f7e1dfcc1d0c9a
montukv <46618399+montukv@users.noreply.github.com>
2020-05-19 00:12:42 +0530

    Add files via upload

---
+++ leetcode/sameTree.py
@@ -0,0 +1,37 @@
+'''100. Same Tree
+Easy
+
+1875
+
+56
+
+Add to List
+
+Share
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false'''
+def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p is q
+        
