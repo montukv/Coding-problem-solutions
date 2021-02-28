8fcd6fd5ee312c677a615c14c1483847c36d27c1
Montukeshwar Vaishnaw <46618399+montukv@users.noreply.github.com>
2021-02-28 20:01:37 +0530

    Add files via upload

---
+++ Data structure and algorithms/reverseTheArray.cpp
@@ -0,0 +1,130 @@
+/*
+Write a program to reverse an array or string
+Difficulty Level : Basic
+
+Given an array (or string), the task is to reverse the array/string.
+Examples :
+
+
+Input  : arr[] = {1, 2, 3}
+Output : arr[] = {3, 2, 1}
+
+Input :  arr[] = {4, 5, 1, 2}
+Output : arr[] = {2, 1, 5, 4}
+
+*/
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)   scanf("%d",&x)
+#define sl(x)   scanf("%lld",&x)
+#define ss(s)   scanf("%s",s)
+#define pi(x)   printf("%d\n",x)
+#define pl(x)   printf("%lld\n",x)
+#define ps(s)   printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb              push_back
+#define mp              make_pair
+#define F               first
+#define S               second
+#define all(x)          x.begin(), x.end()
+#define clr(x)          memset(x, 0, sizeof(x))
+#define sortall(x)      sort(all(x))
+#define tr(it, a)       for(auto it = a.begin(); it != a.end(); it++)
+#define PI              3.1415926535897932384626
+#define po(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define vfor(v)           for (int i = 1; i < v.size(); i++)        // for iterating in a vector 
+#define vsort(v)        sort(v.begin(), v.end());           // for sorting a vector
+#define chartonum(str,i)    (str[i] & NUM);   // print position of a char in alaphabet
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>    pl;
+typedef vector<int>     vi;
+typedef vector<ll>      vl;
+typedef vector<pii>     vpii;
+typedef vector<pl>      vpl;
+typedef vector<vi>      vvi;
+typedef vector<vl>      vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+    uniform_int_distribution<int> uid(0, lim - 1);
+    return uid(rang);
+}
+int mpow(int base, int exp);
+void ipgraph(int n, int m);
+void dfs(int u, int par);
+
+//const int mod = 1'000'000'007;
+const int N = 3e5, M = N , NUM = 31;;
+//=======================
+
+vi g[N];
+int a[N];
+void solve() {
+    vi array;
+    int n;
+    while (cin >> n)
+    {
+        array.push_back(n);
+    }
+    reverse(array.begin(), array.end());
+    for (int i = 0 ; i < array.size(); i++)
+    {
+        cout << array[i] << " " ;
+    }
+
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    int t = 1;
+    //cin >> t;
+    int c;
+
+    while (t--) {
+        solve();
+
+
+    }
+    return 0;
+}
+/*
+int mpow(int base, int exp) {
+  base %= mod;
+  int result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = ((ll)result * base) % mod;
+    base = ((ll)base * base) % mod;
+    exp >>= 1;
+  }
+  return result;
+}
+*/
+void ipgraph(int n, int m) {
+    int i, u, v;
+    while (m--) {
+        cin >> u >> v;
+        u--, v--;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+}
+
+void dfs(int u, int par) {
+    for (int v : g[u]) {
+        if (v == par) continue;
+        dfs(v, u);
+    }
+}
