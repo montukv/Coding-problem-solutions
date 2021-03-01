5d20628bb8bef9aefb2accca6b3ec33476d06a7f
Montukeshwar Vaishnaw <46618399+montukv@users.noreply.github.com>
2021-03-01 11:58:01 +0530

    Add files via upload

---
+++ DSA Practice/KthSmallestNumber.cpp
@@ -0,0 +1,141 @@
+/*
+Kth smallest element
+Medium Accuracy: 46.66% Submissions: 28414 Points: 4
+Given an array arr[] and a number K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+
+Example 1:
+
+Input:
+N = 6
+arr[] = 7 10 4 3 20 15
+K = 3
+Output : 7
+Explanation :
+3rd smallest element in the given
+array is 7.
+Example 2:
+
+Input:
+N = 5
+arr[] = 7 10 4 20 15
+K = 4
+Output : 15
+Explanation :
+4th smallest element in the given
+array is 15.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)   scanf("%d",&x)
+#define sl(x)   scanf("%lld",&x)
+#define ss(s)   scanf("%s",s)
+#define pi(x)   printf("%d\n",x)
+#define pl(x)   printf("%lld\n",x)
+#define ps(s)   printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb              push_back
+#define mp              make_pair
+#define F               first
+#define S               second
+#define all(x)          x.begin(), x.end()
+#define clr(x)          memset(x, 0, sizeof(x))
+#define sortall(x)      sort(all(x))
+#define tr(it, a)       for(auto it = a.begin(); it != a.end(); it++)
+#define PI              3.1415926535897932384626
+#define po(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define vfor(v)           for (int i = 1; i < v.size(); i++)        // for iterating in a vector 
+#define vsort(v)        sort(v.begin(), v.end());           // for sorting a vector
+#define chartonum(str,i)    (str[i] & NUM);   // print position of a char in alaphabet
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>    pl;
+typedef vector<int>     vi;
+typedef vector<ll>      vl;
+typedef vector<pii>     vpii;
+typedef vector<pl>      vpl;
+typedef vector<vi>      vvi;
+typedef vector<vl>      vvl;
+typedef priority_queue<int> pq;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+    uniform_int_distribution<int> uid(0, lim - 1);
+    return uid(rang);
+}
+int mpow(int base, int exp);
+void ipgraph(int n, int m);
+void dfs(int u, int par);
+
+//const int mod = 1'000'000'007;
+const int N = 3e5, M = N , NUM = 31;;
+//=======================
+
+vi g[N];
+int a[N];
+void solve() {
+    //Max heap
+    priority_queue<int> q;
+    int n, k, a;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        q.push(a);
+    }
+    cin >> k;
+    for (int i = 0; i < k; ++i)
+    {
+        q.pop();
+    }
+    cout << q.top();
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    int t = 1;
+    //cin >> t;
+    int c;
+
+    while (t--) {
+        solve();
+
+
+    }
+    return 0;
+}
+/*
+int mpow(int base, int exp) {
+  base %= mod;
+  int result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = ((ll)result * base) % mod;
+    base = ((ll)base * base) % mod;
+    exp >>= 1;
+  }
+  return result;
+}
+*/
+void ipgraph(int n, int m) {
+    int i, u, v;
+    while (m--) {
+        cin >> u >> v;
+        u--, v--;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+}
+
+void dfs(int u, int par) {
+    for (int v : g[u]) {
+        if (v == par) continue;
+        dfs(v, u);
+    }
+}
