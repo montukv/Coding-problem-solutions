335cae1ee7e7fb44d974eadd239fa65c7cbf0954
Montukeshwar Vaishnaw <46618399+montukv@users.noreply.github.com>
2021-03-02 14:05:47 +0530

    Add files via upload

---
+++ DSA Practice/unionOfTwoArray.cpp
@@ -0,0 +1,132 @@
+/*Union of two arrays
+Basic Accuracy: 52.81% Submissions: 11434 Points: 1
+Given two arrays A and B of size N and M respectively. The task is to find union between these two arrays.
+
+Union of the two arrays can be defined as the set containing distinct elements from both the arrays. If there are repetitions, then only one occurrence of element should be printed in union.
+
+Example 1:
+
+Input:
+5 3
+1 2 3 4 5
+1 2 3
+
+Output:
+5
+
+Explanation:
+1, 2, 3, 4 and 5 are the
+elements which comes in the union set
+of both arrays. So count is 5.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)   scanf("%d",&x)
+#define sl(x)   scanf("%lld",&x)
+#define ss(s)   scanf("%s",s)
+#define pi(x)   printf("%d\n",x)
+#define pl(x)   printf("%lld\n",x)
+#define ps(s)   printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb              push_back
+#define mp              make_pair
+#define F               first
+#define S               second
+#define all(x)          x.begin(), x.end()
+#define clr(x)          memset(x, 0, sizeof(x))
+#define sortall(x)      sort(all(x))
+#define tr(it, a)       for(auto it = a.begin(); it != a.end(); it++)
+#define PI              3.1415926535897932384626
+#define po(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define vfor(i,v)           for (int i = 0; i < v.size(); i++)
+#define chartonum(str,i)    (str[i] & NUM);   // print position of a char in alaphabet
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>    pl;
+typedef vector<int>     vi;
+typedef vector<ll>      vl;
+typedef vector<pii>     vpii;
+typedef vector<pl>      vpl;
+typedef vector<vi>      vvi;
+typedef vector<vl>      vvl;
+typedef unordered_map<int, int> map;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+    uniform_int_distribution<int> uid(0, lim - 1);
+    return uid(rang);
+}
+int mpow(int base, int exp);
+void ipgraph(int n, int m);
+void dfs(int u, int par);
+
+//const int mod = 1'000'000'007;
+const int N = 3e5, M = N , NUM = 31;;
+//=======================
+
+vi g[N];
+int a[N];
+void solve() {
+    int n, m;
+    cin >> n >> m ;
+    int a[n], b[m];
+    unordered_map<int, int> map;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        map[a[i]]++;
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> b[i];
+        map[b[i]]++;
+    }
+    cout << map.size();
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
+/*
+int mpow(int base, int exp) {
+  base %= mod;
+  int result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = ((ll)result * base) % mod;
+    base = ((ll)base * base) % mod;
+    exp >>= 1;
+  }
+  return result;
+}
+*/
+void ipgraph(int n, int m) {
+    int i, u, v;
+    while (m--) {
+        cin >> u >> v;
+        u--, v--;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+}
+
+void dfs(int u, int par) {
+    for (int v : g[u]) {
+        if (v == par) continue;
+        dfs(v, u);
+    }
+}
