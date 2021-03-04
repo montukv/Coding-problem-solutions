4d5978b2863bac3fc38d2f9a001ff1ac8708bf1a
Montukeshwar Vaishnaw <46618399+montukv@users.noreply.github.com>
2021-03-04 22:00:44 +0530

    Add files via upload

---
+++ DSA Practice/mergeWithoutExtraSpace.cpp
@@ -0,0 +1,144 @@
+/*  Solved using insertion sorting technique with o(nxm) time complexity
+Merge Without Extra Space
+Hard Accuracy: 36.41% Submissions: 31071 Points: 8
+Given two sorted arrays arr1[] of size N and arr2[] of size M. Each array is sorted in non-decreasing order. Merge the two arrays into one sorted array in non-decreasing order without using any extra space.
+
+
+Example 1:
+
+Input:
+N = 4, M = 5
+arr1[] = {1, 3, 5, 7}
+arr2[] = {0, 2, 6, 8, 9}
+Output: 0 1 2 3 5 6 7 8 9
+Explanation: Since you can't use any
+extra space, modify the given arrays
+to form
+arr1[] = {0, 1, 2, 3}
+arr2[] = {5, 6, 7, 8, 9}
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)   scanf("%d",&x)
+#define sl(x)   scanf("%lld",&x)
+#define ss(s)   scanf("%s",s)
+#define pi(x)   printf("%d\n",x)
+#define pl(x)   printf("%lld\n",x)
+#define ps(s)   printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb              push_back
+#define mp              make_pair
+#define F               first
+#define S               second
+#define all(x)          x.begin(), x.end()
+#define clr(x)          memset(x, 0, sizeof(x))
+#define sortall(x)      sort(all(x))
+#define tr(it, a)       for(auto it = a.begin(); it != a.end(); it++)
+#define PI              3.1415926535897932384626
+#define po(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define vfor(i,v)           for (int i = 0; i < v.size(); i++)
+#define chartonum(str,i)    (str[i] & NUM);   // print position of a char in alaphabet
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>    pl;
+typedef vector<int>     vi;
+typedef vector<ll>      vl;
+typedef vector<pii>     vpii;
+typedef vector<pl>      vpl;
+typedef vector<vi>      vvi;
+typedef vector<vl>      vvl;
+typedef unordered_map<int, int> map;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+    uniform_int_distribution<int> uid(0, lim - 1);
+    return uid(rang);
+}
+int mpow(int base, int exp);
+void ipgraph(int n, int m);
+void dfs(int u, int par);
+
+//const int mod = 1'000'000'007;
+const int N = 3e5, M = N , NUM = 31;;
+//=======================
+
+vi g[N];
+int a[N];
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    int a[n], b[m];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> b[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > b[0])
+        {
+            swap(a[i] , b[0]);
+            sort(b, b + m);
+        }
+
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << " " << a[i];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cout << " " << b[i];
+    }
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
+/*
+int mpow(int base, int exp) {
+  base %= mod;
+  int result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = ((ll)result * base) % mod;
+    base = ((ll)base * base) % mod;
+    exp >>= 1;
+  }
+  return result;
+}
+*/
+void ipgraph(int n, int m) {
+    int i, u, v;
+    while (m--) {
+        cin >> u >> v;
+        u--, v--;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+}
+
+void dfs(int u, int par) {
+    for (int v : g[u]) {
+        if (v == par) continue;
+        dfs(v, u);
+    }
+}
